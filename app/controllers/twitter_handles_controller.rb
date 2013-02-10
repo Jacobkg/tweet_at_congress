3d486aa9e3348dd84da43c5cc2b0f4454f3f4348
Jacob Green <jacob@nationbuilder.com>
2013-02-09 21:01:05 -0800

    First commit

---
+++ app/controllers/twitter_handles_controller.rb
@@ -0,0 +1,20 @@
+class TwitterHandlesController < ApplicationController
+
+  def index
+  end
+
+  def create
+    api_key = ENV['SUNLIGHT_KEY']
+    url = "http://congress.api.sunlightfoundation.com/legislators/locate?apikey=#{api_key}&zip=#{params[:zip_code]}"
+    response = HTTParty.get(url)
+    representative_hash = response.parsed_response['results'].select {|n| n["chamber"] == "house"}.first
+    twitter_id = representative_hash["twitter_id"]
+    name = representative_hash["first_name"] + " " + representative_hash["last_name"]
+
+    redirect_to tweet_twitter_handles_path(:twitter_handle => twitter_id, :name => name)
+  end
+
+  def tweet
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetAtCongress::Application.config.secret_token = '90dab921a71879d9ccabc2b863cb91217c562a30c674a16efc78adfcd17ece04a98dbaffbc24ccefb1d2586886ccbe4a8f27ffda9d9741c82a84c318737180fe'
