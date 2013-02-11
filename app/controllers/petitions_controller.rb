44c0f6dc786ad0c480e4fe23d1da2cb7706e4bff
Jacob Green <jacob@nationbuilder.com>
2013-02-10 20:07:36 -0800

    Add shortened url.

--- app/controllers/petitions_controller.rb
+++ app/controllers/petitions_controller.rb
@@ -8,6 +8,11 @@ class PetitionsController < ApplicationController
   end
 
   def links
+    @long_link = twitter_handles_url(:text => params[:text])
+    response = HTTParty.post "https://www.googleapis.com/urlshortener/v1/url",
+     :body => {'longUrl' => @long_link}.to_json,
+     :headers => { 'Content-Type' => 'application/json' }
+    @short_link = response.parsed_response["id"]
   end
 
 end
